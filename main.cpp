4945d977f5fc6b3de96421eed8669c008ba27fe1
RemigayloYegor <144631320+RemigayloYegor@users.noreply.github.com>
2025-05-26 17:30:14 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <cmath>      
+
+struct less_abs : public std::binary_function<int, int, bool> 
+{
+    bool operator()(int a, int b) const 
+    {
+        return std::abs(a) < std::abs(b);
+    }
+};
+
+int main()
+{
+    std::vector<int> V = {3, -5, 2, 1, -4, 6, -2};
+    auto it = std::adjacent_find(V.begin(), V.end(), std::not2(less_abs()));
+    if (it != V.end()) 
+    {
+        std::cout << "Найдена пара: (" << *it << ", " << *(it + 1) << ")" << std::endl;
+    } else {
+        std::cout << "0" << std::endl;
+    }
+
+    return 0;
+}
