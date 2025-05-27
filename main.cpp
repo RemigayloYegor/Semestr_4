07698d89db93b7682f93ee4c44407a2ef2371e73
RemigayloYegor <144631320+RemigayloYegor@users.noreply.github.com>
2025-05-27 18:37:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,18 @@
 #include <functional>
 #include <cmath>      
 
-struct less_abs : public std::binary_function<int, int, bool> 
-{
-    less_abs() : std::function<bool(int, int)>([this](int a, int b)
-    {
-        return abs(a) < abs(b);
-    }) {}
-    }
-};
-
 int main()
 {
     std::vector<int> V;
     std::copy(std::istream_iterator<int>(std::cin),std::istream_iterator<int>(),std::back_inserter(V));
+    
+    auto less_abs = [](int a, int b)
+    {
+        return std::abs(a) < std::abs(b);
+    };
+    
     auto it = std::adjacent_find(V.begin(), V.end(), std::not2(less_abs()));
+    
     if (it != V.end()) 
     {
         std::cout << "Найдена пара: (" << *it << ", " << *(it + 1) << ")" << std::endl;
