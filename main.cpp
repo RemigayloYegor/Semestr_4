d6f1f12cc990215b43af26217c7cb8bab4cb0ca3
RemigayloYegor <144631320+RemigayloYegor@users.noreply.github.com>
2025-05-27 18:53:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,19 +3,20 @@
 #include <algorithm>
 #include <functional>
 #include <cmath>      
+#include <iterator> 
 
 int main()
 {
     std::vector<int> V;
     std::copy(std::istream_iterator<int>(std::cin),std::istream_iterator<int>(),std::back_inserter(V));
-    
+
     auto less_abs = [](int a, int b)
     {
         return std::abs(a) < std::abs(b);
     };
-    
-    auto it = std::adjacent_find(V.begin(), V.end(), std::not2(less_abs()));
-    
+
+    auto it = std::adjacent_find(V.begin(), V.end(),  [&less_abs](int a, int b) { return !less_abs(a, b); });
+
     if (it != V.end()) 
     {
         std::cout << "Найдена пара: (" << *it << ", " << *(it + 1) << ")" << std::endl;
